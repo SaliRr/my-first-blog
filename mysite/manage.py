f67e13f337b982ee7bb3c5fcfdd03a488469a8c9
SaliRr <45199110+SaliRr@users.noreply.github.com>
2019-05-05 22:24:17 +0400

    Update manage.py

--- mysite/manage.py
+++ mysite/manage.py
@@ -1,22 +1,77 @@
-#!/usr/bin/env python
-import os
-import sys
+import random, re
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError:
-        # The above import may fail for some other reason. Ensure that the
-        # issue is really that Django is missing to avoid masking other
-        # exceptions on Python 2.
-        try:
-            import django
-        except ImportError:
-            raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            )
-        raise
-    execute_from_command_line(sys.argv)
+BIG_SIGN = list('QWERTYUIOPASDFGHJKLZXCVBNM')
+LOW_SIGN = list('qwertyuiopasdfghjklzxcvbnm')
+NUMS = list('1234567890')
+LETTER_RANDOM = list('1234567')
+NUMBER_RANDOM = list('123')
+key_num = ''
+key_num2 = ''
+key_num3 = ''
+keyA = ""
+key_big = ''
+key_big2 = ''
+key_big3 = ''
+keyA2 = ''
+keyA3 = ''
+with open('steam-keys.txt', 'w', encoding='utf-8') as file_keys:
+    for i in range(50):
+        letter_big = random.randint(6, 20)
+        for i in range(letter_big):
+            value = random.randrange(len(BIG_SIGN))
+            value2 = random.randrange(len(BIG_SIGN))
+            value3 = random.randrange(len(BIG_SIGN))
+            key_big += BIG_SIGN[value]
+            key_big2 += BIG_SIGN[value2]
+            key_big3 += BIG_SIGN[value3]
+
+        nums = random.randint(6, 9)
+        for i in range(nums):
+            value = random.randrange(len(NUMS))
+            value2 = random.randrange(len(NUMS))
+            value3 = random.randrange(len(NUMS))
+            key_num += NUMS[value]
+            key_num2 += NUMS[value2]
+            key_num3 += NUMS[value3]
+
+        keyA = key_num + key_big
+        keyA2 = key_num2 + key_big2
+        keyA3 = key_num3 + key_big3
+        list_of_keys = list(keyA)
+        list_of_keys2 = list(keyA2)
+        list_of_keys3 = list(keyA3)
+        random.shuffle(list_of_keys)
+        random.shuffle(list_of_keys2)
+        random.shuffle(list_of_keys3)
+        append_string = ""
+        append_string2 = ""
+        append_string3 = ""
+
+        for i in range(5):
+            append_string += list_of_keys[i]
+        for i in range(5):
+            append_string2 += list_of_keys2[i]
+        for i in range(5):
+            append_string3 += list_of_keys3[i]
+        l = re.findall('\d+', append_string)
+
+        wow = ""
+        wow += append_string
+        wow += '-'
+        wow += append_string2
+        wow += '-'
+        wow += append_string3
+        #wow += ':'
+        wow += '\n'
+
+
+
+        file_keys.write(wow)
+
+
+        print(wow)
+
+
+
+
+#print(append_string)
